321a61def4990f783fb3b927da4cbf416846b246
Vladislav Baranov <briliant645@gmail.com>
2024-10-05 22:43:17 +0300

    deploy backend

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      "pre-deploy-local": `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH} -i ${DEPLOY_KEY}`,
+      "pre-deploy-local": `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       "post-deploy":
         "cd backend && pwd && npm ci && npm run build startOrRestart ecosystem.config.js --env production",
     },
