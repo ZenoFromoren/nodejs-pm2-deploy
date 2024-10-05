103ab69a8e98015bac56d4c82743ba84606cc42e
Vladislav Baranov <briliant645@gmail.com>
2024-10-05 22:10:48 +0300

    deploy backend

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: ".env.deploy" });
 
-const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF } =
+const { DEPLOY_KEY, DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF } =
   process.env;
 
 module.exports = {
@@ -13,14 +13,15 @@ module.exports = {
   ],
   deploy: {
     production: {
+      key: DEPLOY_KEY,
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      "pre-deploy-local": `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       "post-deploy":
-        "cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
+        "cd backend && pwd && npm ci && npm run build startOrRestart ecosystem.config.js --env production",
     },
   },
 };
